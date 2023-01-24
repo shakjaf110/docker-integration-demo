64613a4001b79745d04b0e39b1a653a6a0bc7bbf
shakeeb raza <shakjaf110@gmail.com>
2023-01-24 19:01:29 +0500

    added kotlin in spring boot

---
+++ src/main/java/com/example/demo/controller/KotlinTestController.kt
@@ -0,0 +1,36 @@
+package com.example.demo.controller
+
+import com.example.demo.domain.Test
+import com.example.demo.repo.KotlinTestRepository
+import com.example.demo.repo.TestRepo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@Controller
+class KotlinTestController {
+
+    @Autowired
+    var testRepo: TestRepo? = null
+
+    @Autowired
+    var kotlinTestRepository: KotlinTestRepository? = null
+
+    @GetMapping("/kotlin/test/all")
+    fun getCall():ResponseEntity<List<Test>> {
+        val tests=testRepo?.findAll()
+        val testResponseEntity= ResponseEntity<List<Test>>(tests,HttpStatus.FOUND);
+        return testResponseEntity;
+    }
+
+    @GetMapping("/kotlin/test/{id}")
+    fun getCall(@PathVariable id:Int):ResponseEntity<Test> {
+        var test=kotlinTestRepository?.findById(id)?.get()
+        var testResponseEntity= ResponseEntity<Test>(test,HttpStatus.FOUND);
+        return testResponseEntity;
+    }
+
+}
